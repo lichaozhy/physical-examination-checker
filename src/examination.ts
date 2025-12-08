1605615a2c4c368692e0127d56b99169b41ad01a
chaoslee <chaos2lee@qq.com>
2025-12-08 11:43:39 +0800

    [ADDED] Page::IndexPage

--- src/examination.ts
+++ src/examination.ts
@@ -1,35 +1,93 @@
 import * as XLSX from 'xlsx';
 
+function toRenderValue(raw: string) {
+	if (raw === '男') {
+		return 1;
+	}
+
+	if (raw === '女') {
+		return 2;
+	}
+
+	return 0;
+}
+
+function toNumberValue(raw: string) {
+	if (raw) {
+		return Number(raw);
+	}
+
+	return null;
+}
+
 export async function read(file: File) {
 	const workbook = XLSX.read(await file.arrayBuffer());
+	const [headSheetName] = workbook.SheetNames;
 
-	return workbook;
+	if (headSheetName === undefined) {
+		return null;
+	}
+
+	const worksheet = workbook.Sheets[headSheetName]!;
+	const [, ...dataRows] = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+
+	return {
+		items: function *items(): Generator<ItemData> {
+			for (const row of dataRows as string[][]) {
+				yield {
+					name: row[0]!,
+					render: toRenderValue(row[1]!),
+					id: row[2]!,
+					at: new Date(row[3]!),
+					by: row[4]!,
+					metric: {
+						HGB: toNumberValue(row[5]!),
+						PLT: toNumberValue(row[6]!),
+						WBC: toNumberValue(row[7]!),
+						'NEUT#': toNumberValue(row[8]!),
+						'LYMPH#': toNumberValue(row[9]!),
+						'MID#': toNumberValue(row[10]!),
+						HbA1c: toNumberValue(row[11]!),
+						ALT: toNumberValue(row[12]!),
+						AST: toNumberValue(row[13]!),
+						TBIL: toNumberValue(row[14]!),
+						Cr: toNumberValue(row[15]!),
+						BUN: toNumberValue(row[16]!),
+						TC: toNumberValue(row[17]!),
+						TG: toNumberValue(row[18]!),
+						'LDL-C': toNumberValue(row[19]!),
+						'HDL-C': toNumberValue(row[20]!),
+					},
+				};
+			}
+		},
+	};
 }
 
 type ItemValue = number | null;
 
 export interface ItemData {
 	name: string;
-	render: 0 | 1 | null;
+	render: 0 | 1 | 2;
 	id: string;
 	at: Date;
 	by: string;
 	metric: {
-		'HGB': ItemValue;
-		'PLT': ItemValue;
-		'WBC': ItemValue;
+		HGB: ItemValue;
+		PLT: ItemValue;
+		WBC: ItemValue;
 		'NEUT#': ItemValue;
 		'LYMPH#': ItemValue;
 		'MID#': ItemValue;
-		'HbA1c': ItemValue;
-		'ALT': ItemValue;
-		'AST': ItemValue;
-		'TBIL': ItemValue;
-		'Cr': ItemValue;
-		'BUN': ItemValue;
-		'TC': ItemValue;
-		'TG': ItemValue;
+		HbA1c: ItemValue;
+		ALT: ItemValue;
+		AST: ItemValue;
+		TBIL: ItemValue;
+		Cr: ItemValue;
+		BUN: ItemValue;
+		TC: ItemValue;
+		TG: ItemValue;
 		'LDL-C': ItemValue;
 		'HDL-C': ItemValue;
-	}
+	};
 }
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -4,54 +4,173 @@
 		padding
 	>
 		<div class="app-content-main">
-			<q-form>
-				<div class="row q-col-gutter-md">
-					<div class="col">
-						<q-file
-							stack-label
-							square
-							:label="$t('parameter.file')"
-							dense
-							outlined
-							v-model="file"
-						></q-file>
-					</div>
-					<div class="col-shrink">
-						<q-input
-							type="number"
-							style="width: 10em"
-							stack-label
-							square
-							:label="$t('parameter.repeat-metric-number')"
-							dense
-							outlined
-							v-model="repeatMetricNumber"
-						></q-input>
-					</div>
-					<div class="col-12 row justify-center">
-						<div
-							class="col"
-							style="max-width: 24em"
-						>
+			<div class="row q-col-gutter-md">
+				<div class="col-2">
+					<q-form>
+						<div>
+							<q-file
+								stack-label
+								square
+								:label="$t('parameter.file')"
+								dense
+								outlined
+								v-model="sampleFile"
+								clearable
+								color="indigo-10"
+							></q-file>
+						</div>
+						<div class="q-pt-lg q-mt-md">
+							<q-slider
+								type="number"
+								label
+								snap
+								label-always
+								:min="0"
+								:max="10"
+								:label-value="`${$t('parameter.repeat-metric-number')}: ${repeatMetricNumber}`"
+								track-size="10px"
+								markers
+								v-model="repeatMetricNumber"
+								color="indigo-10"
+							></q-slider>
+						</div>
+						<div>
 							<q-btn
-								:disable="file === null"
+								:disable="!ready"
 								dense
 								class="full-width"
-								size="lg"
-								color="primary"
+								size="md"
+								push
+								color="indigo-10"
 								:label="$t('check')"
+								@click="check"
 							></q-btn>
 						</div>
+						<div>
+							<q-linear-progress
+								:value="progress.done / progress.total"
+								class="q-mt-md"
+							/>
+							{{ progress }}
+						</div>
+					</q-form>
+				</div>
+				<div class="col-10 row q-col-gutter-md">
+					<div
+						v-for="(
+							{ reference: r, contrast: c, metrics }, i
+						) in similarResultList"
+						:key="i"
+						class="col-12"
+					>
+						<q-card
+							square
+							flat
+							bordered
+						>
+							<q-table
+								dense
+								hide-pagination
+								square
+								flat
+								bordered
+								separator="cell"
+								:rows="[
+									{ id: r.id, name: r.name, by: r.by, ...r.metric },
+									{ id: c.id, name: c.name, by: c.by, ...c.metric },
+								]"
+							>
+								<template #body-cell="props">
+									<q-td
+										:props="props"
+										:class="{
+											'bg-red-2': metrics[props.col.name as MetricName],
+										}"
+										>{{ props.value }}</q-td
+									>
+								</template>
+							</q-table>
+						</q-card>
 					</div>
 				</div>
-			</q-form>
+			</div>
 		</div>
 	</q-page>
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
+import * as Examination from 'src/examination';
+
+type MetricName = keyof Examination.ItemData['metric'];
+
+interface SimilarResult {
+	reference: Examination.ItemData;
+	contrast: Examination.ItemData;
+	metrics: Partial<Record<MetricName, true>>;
+}
 
 const repeatMetricNumber = ref<number>(6);
-const file = ref<File | null>(null);
+const sampleFile = ref<File | null>(null);
+const similarResultList = ref<SimilarResult[]>([]);
+const progress = ref({ done: 0, total: 1 });
+
+const ready = computed(() => {
+	return repeatMetricNumber.value >= 0 && sampleFile.value !== null;
+});
+
+async function check() {
+	const MIN_REPEAT_NUMBER = repeatMetricNumber.value;
+	const FILE = sampleFile.value;
+
+	if (FILE === null) {
+		return;
+	}
+
+	const result = await Examination.read(FILE);
+
+	if (result === null) {
+		return;
+	}
+
+	const dataItemList = [...result.items()];
+	const length = dataItemList.length;
+
+	similarResultList.value = [];
+	progress.value.total = (length * (length - 1)) / 2;
+
+	for (let i = 0; i < length; i++) {
+		const reference = dataItemList[i]!;
+
+		for (let j = i + 1; j < length; j++) {
+			const contrast = dataItemList[j]!;
+			const similarMetrics: Partial<Record<MetricName, true>> = {};
+
+			for (const key in contrast.metric) {
+				const referenceValue = reference.metric[key as MetricName];
+				const contrastValue = contrast.metric[key as MetricName];
+
+				if (referenceValue === null || contrastValue === null) {
+					continue;
+				}
+
+				if (Math.abs(referenceValue - contrastValue) < 0.005) {
+					similarMetrics[key as MetricName] = true;
+				}
+			}
+
+			if (Object.keys(similarMetrics).length >= MIN_REPEAT_NUMBER) {
+				similarResultList.value.push({
+					reference,
+					contrast,
+					metrics: similarMetrics,
+				});
+			}
+
+			progress.value.done++;
+		}
+
+		await new Promise((resolve) => setTimeout(resolve, 1));
+	}
+}
 </script>
